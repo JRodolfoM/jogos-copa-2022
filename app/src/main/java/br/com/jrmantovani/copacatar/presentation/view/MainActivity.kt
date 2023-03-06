9b8d609df26d002ef9760538392a406499956693
JRodolfoM <j_rodolfom@live.com>
2023-03-05 21:58:16 -0300

    primeiro commit configuração

---
+++ app/src/main/java/br/com/jrmantovani/copacatar/presentation/view/MainActivity.kt
@@ -0,0 +1,43 @@
+package br.com.jrmantovani.copacatar.presentation.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.jrmantovani.copacatar.ui.theme.CopaCatarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CopaCatarTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CopaCatarTheme {
+        Greeting("Android")
+    }
+}
